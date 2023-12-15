5ee603e418f0daad53dbfc516c8dd76a520d0bf3
TersMorder <nice.alpaka@gmail.com>
2023-12-15 19:25:38 +0300

    Убраны изменения окончаний, изменена проверка наличия красок в инвентаре на более правильную, исправлена проблема нейминга класса Object, убраны лишние классы коротышек, которые не имели своих методов

--- src/Interfaces/NameChanging.java
+++
@@ -1,8 +0,0 @@
-package Interfaces;
-/** Используется для изменения имени и отчистки его изменений у коротышек и объектов*/
-public interface NameChanging {
-    /** Меняет окончание у имени на подходящее контексту*/
-    void changeNameEnding();
-    /** Меняет имя на прежнее */
-    void clearNameChanges();
-}
--- src/Objects/Paints.java
+++ src/Objects/Paints.java
@@ -1,12 +1,2 @@
-package Objects;
-
-public class Paints extends Object{
-    final private String oldName;
-
-    public Paints(String name){
-        super(name);
-        this.name = name;
-        oldName = name;
-    }
-
+package Objects;public class Paints {
 }
